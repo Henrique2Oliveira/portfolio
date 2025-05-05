77facfc844823755bbafbd68e443e697623996d3
Henrique2Oliveira <henriqueoliveira_2021@outlook.com>
2025-05-05 10:55:58 +0000

    change folder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,6 @@ function Header() {
       <h1 className="text-2xl font-bold md:text-4xl">Henrique Oliveira</h1>
       <p className="rounded-full bg-secondary w-fit px-3 italic">FullStack Engineer </p>
       <p className="text-sm">Azores, Portugal</p>
-
       <button className="rounded-full bg-white hover:bg-secondary hover:text-white w-fit text-black px-5 py-2 shadow-[-1px_6px_0px_0px_rgba(0,_0,_0,_0.9)] hover:px-6 transition-all duration-100 ease-in "><a href="#" >Contact me</a></button>
 
     </header>
